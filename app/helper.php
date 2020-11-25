e8d24ae34ebf1624749510382b4c172c8299982b
itssonu <59788324+itssonu@users.noreply.github.com>
2020-11-25 15:30:02 +0530

    Update helper.php

--- app/helper.php
+++ app/helper.php
@@ -1,3 +1,4 @@
+<?php
 function getDistanceBetweenPoints($lat1, $lon1, $lat2, $lon2) {
     $theta = $lon1 - $lon2;
     $miles = (sin(deg2rad($lat1)) * sin(deg2rad($lat2))) + (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)));
